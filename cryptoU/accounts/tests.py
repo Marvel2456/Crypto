a9cd747bc84023e2a3ff986d40fca77c70933e5e
Marvel2456 <marvellousidemudia@gmail.com>
2023-04-12 12:44:32 +0100

    I created the accounts apps and setup bootstrap

---
+++ cryptoU/accounts/tests.py
@@ -0,0 +1,14 @@
+# from django.test import TestCase
+# from django.urls import reverse
+# from http import HTTPStatus
+
+# # Create your tests here.
+
+# class AccountCreationTest(TestCase):
+#     # Here i will write the test for the sign up page.
+
+#     def test_signup_page_exist(self):
+#         response = self.client(reverse('signup_page'))
+
+#         self.assertEqual(response.status_code, HTTPStatus.OK)
+#         self.assertTemplateUsed('accounts/signup.html')
